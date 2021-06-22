beacd465d5586072846b0f333a40e3745db145ed
charliekranz <charliekranz3@gmail.com>
2021-06-22 11:00:57 +0000

    trying to force settings py commit

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-n0*u#&^nu6vtsf!3h4t4w@3esdcp&(3o%k9+xr6j0s*xyz+8%e')
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
